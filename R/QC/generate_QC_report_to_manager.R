d0e83959fffd4510537b4041273a08d5ace03dcf
Ilkka Havukkala <ilkka.havukkala@mbie.govt.nz>
2016-04-18 12:19:24 +1200

    QC report started

---
+++ R/QC/generate_QC_report_to_manager.R
@@ -0,0 +1,19 @@
+##  Purpose: To generate a QC report for Mark Hatcher's export intelligence reports repository
+##           at P:\OTSP\export_intelligence
+##  Author: Ilkka Havukkala
+##  Date: 14 April 2016
+
+##  Inputs: TRED import/export data of commodities
+##  Outputs: decision to publish data
+
+##  Dependencies: needs to run first script "analysis_code/checks_QC_prod.r"
+##  Modified by: 
+##  Peer reviewed by : 
+
+## first round QC report
+rmarkdown::render("R/QC/Quality_Control_export_intelligence.Rmd") 
+
+## Reply from Peter saved in the folder
+
+## final response/action 
+## rmarkdown::render("") 
--- integrate.R
+++ integrate.R
@@ -28,6 +28,7 @@
 #                 limits the set of products to just Salmon and Honey.  The user can uncomment this line to run the program for
 #                 all products.
 
+# Peer review: Ilkka Havukkala 18 April in progress
 
 
 ### clear working space
@@ -61,33 +62,51 @@ options(scipen = 999)
 # set work directory -
 PROJHOME <- getwd()
 
+######
+## functions setup
+######
 
 # read in data / concordances
 source('R/fn_read_product_codes.R')
+
 # looks up units
 source('R/utilities/fn_read_unit_lookup.R')
+
 # cleans up the source data
 source('R/grooming/fn_country_mapping.R')
+
 # removes all files from /output/*
 source('R/utilities/fn_remove_files_from_output_dir.R')
+
 # prints a status message to the screen
 source("R/fn_message_log.R")
+
 # creates a PDF file for a specific product 
 source('R/fn_wrapper_create_product_report.R')
+
 # loads and formats the data file.  Columns are renmaed to suit this code
 source('R/grooming/fn_read_data_and_convert_units.R')
+
 # this creates a cover page with a month ending title
 source('R/create_cover_page/fn_create_pdf_cover_page.R')
+
 # once we have generated the pdfs, this joins them all together
 source('R/create_pdf_compile/fn_create_pdf_compilation.R')
+
 # create .gitignores for each directory in output
 source('R/fn_create_git_ignores.R')
+
+########
+# Processing starts from here
+########
+
 
 # read in data, convert the units and renname a few countries
 df_me_exports <- fn_read_data_and_convert_units() %>% fn_country_mapping()
 
 # read products and their codes
 lst_prod_codes <- fn_read_product_codes()
+
 # exclude some products.  These have more than 1 measurement unit type
 vct_exclusions <- c("spirits", "beer", "cider_alcoholic")
 lst_prod_codes <- lst_prod_codes[!names(lst_prod_codes) %in% vct_exclusions]
@@ -128,11 +147,6 @@ glob.env$vct_disp_dates <- c(today = format(Sys.Date(), "%B %d, %Y"),
 
 
 
-
-
-
-
-
 #fn_wrapper_create_product_report("peas")
 
 # invisible() suppresses output from lapply
@@ -150,8 +164,11 @@ invisible(
 # create the cover page
 invisible(fn_create_pdf_cover_page(str_month_end))
 str_file_suffix <- paste0(format(dte_end_date, "%Y"),"-", format(dte_end_date, "%m"))
+
 # the following joins up the cover page with individual product pdfs into a single conscolidated document.
 fn_create_pdf_compilation(glob.env$vct_pdf_names, str_file_suffix)
+
 # create .gitigores for each sub-directory in outputs/
 fn_create_git_ignores()
+
 
